db0c908448f40f201d9fed532fdd814cde7b9f0e
Josephine Mui <josephinemui@yahoo.com>
2026-03-11 13:44:53 -0500

    Added code

--- simple_speech2text.py
+++ simple_speech2text.py
@@ -3,8 +3,8 @@ import requests
 # URL of the audio file to be downloaded
 url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-GPXX04C6EN/Testing%20speech%20to%20text.mp3"
 
-# Path where the audio file will be saved which is one level up from the current directory
-audio_file_path = '../audio_file.mp3'
+# Path where the audio file will be saved in the current directory
+audio_file_path = 'audio_file.mp3'
 
 def download_audio_file(url, file_path):
     try:
@@ -31,3 +31,6 @@ def download_audio_file(url, file_path):
         print(f"Audio file downloaded successfully and saved to {file_path}")
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while downloading the audio file: {e}")
+
+# Call the function to download the audio file
+download_audio_file(url, audio_file_path)
